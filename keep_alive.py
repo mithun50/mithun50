3d16071cebddf610c6e5a09f112a2480748331f6
MithunGowda.B <99024517+mithun50@users.noreply.github.com>
2024-05-12 10:25:04 +0530

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 from threading import Thread
 
 app = Flask(__name__)
 
+# Function to read the contents of db.txt
+def read_db_content():
+    with open('db.txt', 'r') as file:
+        return file.read()
+
+# Route to display the contents of db.txt
 @app.route('/')
 def index():
-    return "MithunGowda.B. Manvanth. KannadaGamrsHub"
+    db_content = read_db_content()
+    return render_template('display_db.html', db_content=db_content)
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():  
     t = Thread(target=run)
